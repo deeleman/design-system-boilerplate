d2c61fc9ae0bf83a6729bd1bb71e84ae6283cd0a
Pablo Deeleman <deeleman@gmail.com>
2022-12-08 04:05:14 +0100

    feat: stack component

---
+++ src/components/hs-stack/hs-stack.tsx
@@ -0,0 +1,51 @@
+import { Component, h, Prop } from '@stencil/core';
+import { css } from '@emotion/css';
+import { SpacingLarge, SpacingSmall } from '../../design-tokens/js/variables.js';
+
+/**
+ * The `<hs-stack>` component is a layout component providing horizontal and vertical
+ * flow capabilities for all its children components and elements.
+ * 
+ * ### Example
+ * ```html
+ * <hs-stack orientation="vertical" gap="small">
+ *  <img src="test.png" />
+ *  <button>Save</button>
+ * </hs-stack>
+ * ```
+ */
+@Component({
+  tag: 'hs-stack',
+  styleUrl: 'hs-stack.css',
+  shadow: false,
+  scoped: true,
+})
+export class HsStack {
+  /**
+   * The `orientation` property sets the direction for the flow,
+   * either vertical or horizontal.
+   */
+  @Prop({ reflect: true })
+  orientation: 'vertical' | 'horizontal'  = 'vertical';
+
+  /**
+   * The `gap` property sets the spacing in between elements, and has no effect
+   * in the leading or trailing element.
+   */
+  @Prop({ reflect: true })
+  gap: 'large' | 'small' = 'small';
+
+  stackStyle = css`
+    gap: ${ this.gap === 'large' ? SpacingLarge.bottom : SpacingSmall.bottom}px;
+    flex-direction: ${this.orientation === 'horizontal' ? 'row' : 'column'};
+  `;
+
+  render() {
+    return (
+      <div class={this.stackStyle}>
+        <slot></slot>
+      </div>
+    );
+  }
+
+}
---
+++ src/components/hs-stack/readme.md
@@ -0,0 +1,31 @@
+# hs-stack
+
+
+
+<!-- Auto Generated Below -->
+
+
+## Overview
+
+The `<hs-stack>` component is a layout component providing horizontal and vertical
+flow capabilities for all its children components and elements.
+
+### Example
+```html
+<hs-stack orientation="vertical" gap="small">
+ <img src="test.png" />
+ <button>Save</button>
+</hs-stack>
+```
+
+## Properties
+
+| Property      | Attribute     | Description                                                                                                    | Type                         | Default      |
+| ------------- | ------------- | -------------------------------------------------------------------------------------------------------------- | ---------------------------- | ------------ |
+| `gap`         | `gap`         | The `gap` property sets the spacing in between elements, and has no effect in the leading or trailing element. | `"large" \| "small"`         | `'small'`    |
+| `orientation` | `orientation` | The `orientation` property sets the direction for the flow, either vertical or horizontal.                     | `"horizontal" \| "vertical"` | `'vertical'` |
+
+
+----------------------------------------------
+
+*Built with [StencilJS](https://stenciljs.com/)*
